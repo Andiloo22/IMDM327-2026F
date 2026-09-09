eb74b7525b076bf9fd285875972c9e9cf57f462c
Myungin Lee <benedic.lee@gmail.com>
2026-09-08 23:01:19 -0400

    Initial commit

---
+++ Assets/327_CourseMaterial/Scripts/ThreeBody.cs
@@ -0,0 +1,90 @@
+// 3-body Starter Code
+// Fall 2026. IMDM 327
+// Instructor. Myungin Lee
+using UnityEngine;
+
+public class ThreeBody : MonoBehaviour
+{
+    private const float G = 500f; // Gravitational constant for this simulation, not the real-world value.
+    BodyProperty[] bp;
+    private int numberOfSphere = 3;
+    class BodyProperty // why struct?
+    {                   // https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/choosing-between-class-and-struct
+        public GameObject body;
+        public float mass;
+        public Vector3 velocity;
+        public Vector3 acceleration;
+    }
+
+
+    void Start()
+    {
+        // Allocate an array to store each body's properties.
+        bp = new BodyProperty[numberOfSphere];
+        // Loop generating the gameobject and assign initial conditions (type, position, (mass/velocity/acceleration)
+        for (int i = 0; i < numberOfSphere; i++)
+        {
+            // Our gameobjects are created here:
+            bp[i] = new BodyProperty();
+            bp[i].body = GameObject.CreatePrimitive(PrimitiveType.Sphere); // why sphere? try different options.
+            // https://docs.unity3d.com/ScriptReference/GameObject.CreatePrimitive.html
+
+            // initial conditions
+            float r = 100f;
+            // position is (x,y,z). In this case, I want to plot them on the circle with r
+
+            // ******** Fill in this part ********
+            // bp[i].body.transform.position = new Vector3( ***, *** , 180);
+            // z = 180 places the bodies in front of a camera near the origin looking along +Z. Try other positions too.
+
+            bp[i].velocity = Vector3.zero; // Try different initial condition
+            bp[i].mass = 1; // Simplified. Try different initial condition
+
+
+            // + This is just pretty trails
+            TrailRenderer trailRenderer = bp[i].body.AddComponent<TrailRenderer>();
+            // Configure the TrailRenderer's properties
+            trailRenderer.time = 100.0f;  // Duration of the trail
+            trailRenderer.startWidth = 0.5f;  // Width of the trail at the start
+            trailRenderer.endWidth = 0.1f;    // Width of the trail at the end
+            // a material to the trail
+            trailRenderer.material = new Material(Shader.Find("Sprites/Default"));
+            // Set the colour gradient along the trail.
+            Gradient gradient = new Gradient();
+            Color targetColor = Color.HSVToRGB((float)i / numberOfSphere, 1f, 1f);
+            gradient.SetKeys(
+                new GradientColorKey[] {
+                    new GradientColorKey(Color.white, 0f), // (color, normalized position)
+                    new GradientColorKey(targetColor, 0.8f)
+                },
+                new GradientAlphaKey[] {
+                    new GradientAlphaKey(1f, 0f), // (alpha, normalized position) 
+                    new GradientAlphaKey(0f, 1f)
+                }
+            );
+            trailRenderer.colorGradient = gradient;
+
+        }
+    }
+
+    void Update()
+    {
+        // Loop for N-body gravity
+        // How should we design the loop?
+        for (int i = 0; i < numberOfSphere; i++)
+        {
+            // Something
+        }
+
+    }
+
+    // Gravity Fuction to finish
+    private Vector3 CalculateGravity(Vector3 distanceVector, float m1, float m2)
+    {
+        Vector3 gravity = Vector3.zero; // note this is also Vector3
+                                        // **** Fill in the function below. 
+                                        // gravity = ****;
+        return gravity;
+    }
+}
+
